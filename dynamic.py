fcc71997e2d5e26f8f1efe9f9f4dfc4c71e3f49e
CarolArevalo <are20461@uvg.edu.gt>
2023-04-07 10:47:17 -0600

    Kadane’s Algorithm

--- dynamic.py
+++ dynamic.py
@@ -1,6 +1,27 @@
 from sys import maxsize
  
- 
+
+# dinamico
+def findMaxSubarraySum(X,n):
+    maxSumEnding = [0]*n
+    maxSumEnding[0] = X[0]
+
+    for i in range(1,n):
+        if maxSumEnding[i-1] > 0:
+            maxSumEnding[i] = X[i] + maxSumEnding[i-1]
+        else:
+            maxSumEnding[i] = X[i]
+
+    maxSubarraySum = -maxsize
+    for i in range(n):
+        maxSubarraySum = max(maxSubarraySum, maxSumEnding[i])
+
+    return maxSubarraySum
+
+arr = [2, 3, 4, 5, 7]
+print ("Maximum contiguous sum is", findMaxSubarraySum(arr, len(arr)))
+
+# Kadane’s Algorithm
 def maxSubArraySum(a, size):
  
     max_so_far = -maxsize - 1
